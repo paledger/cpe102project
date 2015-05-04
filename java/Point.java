d59a546b0ad011d629e9564a6b40e317927f99db
paledger <pledgerw@calpoly.edu>
2015-05-04 15:36:25 -0700

    started Point.java methods

--- java/Point.java
+++ java/Point.java
@@ -8,4 +8,37 @@ public class Point
 	    this.x = x;
 	    this.y = y;
 	}
+
+	public double x()
+	{
+		return this.x;
+	}
+
+	public double y()
+	{
+		return this.y;
+	}
+
+	public boolean withinBounds(WorldModel world)
+	{
+	    return (this.x >= 0 && this.x < world.num_cols) &&
+            (this.y >= 0 && this.y < world.num);
+	}
+
+	public boolean isOccupied(WorldModel world)
+	{
+		return (this.withinBounds(world) && 
+			world.occupancy.getCell(this) != Null);
+	}
+
+	public double distanceSq(Point p2)
+	{
+		return (this.x - p2.x())*(this.x - p2.x()) + (this.y - p2.y())*(this.y - p2.y());
+	}
+
+	public boolean adjacent(Point pt2)
+	{
+		return (this.x == pt2.x() && Math.abs(this.y - pt2.y()) == 1) ||
+		    (this.y == pt2.y() && Math.abs(this.x - pt2.x()) == 1);
+	}
 }
