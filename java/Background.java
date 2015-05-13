be80a5cd077cefe822dd80c7422da358a303737b
leewensim <alee229@calpoly.edu>
2015-05-13 16:57:35 -0700

    Added functionality for currentImg and imgs in Background class.

--- java/Background.java
+++ java/Background.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+import processing.core.*;
+
 public class Background
 {
 	protected String name;
+	protected ArrayList<PImage> imgs;
+	protected int currentImg;
 
-	public Background(String name)
+	public Background(String name, ArrayList<PImage> imgs)
 	{
 		this.name = name;
+		this.imgs = imgs;
+		this.currentImg = 0;
 	}
 	
-	//Missing get_images, get_image, schedule_entity, currentImg
+	protected ArrayList<PImage> getImages()
+	{
+		return imgs;
+	}
+	
+	protected PImage getImage()
+	{
+		return imgs.get(this.currentImg);
+	}
+	//schedule_entity
 }
