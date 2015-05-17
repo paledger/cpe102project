1ed0afff2b1b03a698a24678a83a7dc589e91c58
leewensim <alee229@calpoly.edu>
2015-05-17 12:06:55 -0700

    Added all functions in Actionable

--- java/Actionable.java
+++ java/Actionable.java
@@ -1,14 +1,20 @@
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import processing.core.*;
 
 public class Actionable
     extends Entity
 {
 	protected int currentImg;
+	protected ArrayList<Action> pendingActions;
+	
    public Actionable(String name, Point position,int rate, ArrayList<PImage> imgs)
    {
    	super(name, position,rate, imgs);
 		this.currentImg = 0;
+		this.pendingActions = new ArrayList<Action>();
    }
 	
   protected PImage getImage()
@@ -20,7 +26,39 @@ public class Actionable
   {
 	 this.currentImg = (this.currentImg+1) % imgs.size();
   }
-	 
-	 //missing self.pending_actions[], add_pending_action, get_pending_actions
-	 //remove_pending_actions, clear_pending_actions
+  
+  protected ArrayList<Action> getPendingActions()
+  {
+	  if (this instanceof Actionable)
+	  {
+		  return this.pendingActions;
+	  } else 
+	  {
+		  return new ArrayList<Action>();
+	  }
+  }
+  
+  protected void addPendingAction(Action action)
+  {
+	  if (this instanceof Actionable)
+	  {
+		  this.pendingActions.add(action);
+	  }
+  }
+  
+  protected void removePendingAction(Action action)
+  {
+	  if (this instanceof Actionable)
+	  {
+		  this.pendingActions.remove(action);
+	  }
+  }
+  
+  protected void clearPendingActions()
+  {
+	  if (this instanceof Actionable)
+	  {
+		  this.pendingActions = new ArrayList<Action>();
+	  }
+  }
 }
