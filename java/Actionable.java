fc583230bff9eba4e4343094a410106bd0dc5373
leewensim <alee229@calpoly.edu>
2015-05-17 22:21:04 -0700

    Update Actionable.java

--- java/Actionable.java
+++ java/Actionable.java
@@ -1,18 +1,66 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import processing.core.*;
 
 public class Actionable
    extends Entity
 {
 	protected int currentImg;
+	protected ArrayList<Object> pendingActions;
+	
    public Actionable(String name, Point position,int rate, ArrayList<PImage> imgs)
    {
    	super(name, position,rate, imgs);
 		this.currentImg = 0;
+		this.pendingActions = new ArrayList<Object>();
    }
 	
    protected PImage getImage()
    {
+<<<<<<< HEAD
+ 	 return imgs.get(this.currentImg);
+   }
+ 
+  protected void nextImage()
+  {
+	 this.currentImg = (this.currentImg+1) % imgs.size();
+  }
+  
+  protected ArrayList<Object> getPendingActions()
+  {
+	  if (this instanceof Actionable)
+	  {
+		  return this.pendingActions;
+	  } else 
+	  {
+		  return new ArrayList<Object>();
+	  }
+  }
+  
+  protected void addPendingAction(Object action)
+  {
+	  if (this instanceof Actionable)
+	  {
+		  this.pendingActions.add(action);
+	  }
+  }
+  
+  protected void removePendingAction(Object action)
+  {
+	  if (this instanceof Actionable)
+	  {
+		  this.pendingActions.remove(action);
+	  }
+  }
+  
+  protected void clearPendingActions()
+  {
+	  if (this instanceof Actionable)
+	  {
+		  this.pendingActions = new ArrayList<Object>();
+	  }
+  }
+=======
 	   return imgs.get(this.currentImg);
    }
  
@@ -23,4 +71,5 @@ public class Actionable
 	 
 	 //missing self.pending_actions[], add_pending_action, get_pending_actions
 	 //remove_pending_actions, clear_pending_actions
+>>>>>>> 8f0212ea702583ad119490547cbaf38f4351217d
 }
