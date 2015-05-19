72af048c1121a0327fc2c1312565176d51c10529
paledger <pledgerw@calpoly.edu>
2015-05-18 22:48:46 -0700

    added ImageSgtore Hashmap

--- java/ImageStore.java
+++ java/ImageStore.java
@@ -1,8 +1,12 @@
 import processing.core.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.io.*;
+import java.util.HashMap.*;
+import java.util.List;
+import java.util.Map;
 
 public class ImageStore
 	extends PApplet
@@ -10,6 +14,94 @@ public class ImageStore
 	private final static String DEFAULT_IMAGE_NAME = "background_default";
 	private final static int[] DEFAULT_IMAGE_COLOR = {128, 128, 128, 0};
 
+/*
+	public List<String> readLines() 
+	{
+		File file = new File("imagelist");
+		FileReader fr = new FileReader(file);
+	   try(BufferedReader br = new BufferedReader(fr))
+	   {
+			String line;
+			List<String> output = new LinkedList<String>();
+			while((line = br.readLine()) != null)
+			{
+				output.add(line);
+			}
+			return output;
+		}
+	}
+*/
+ 	private static HashMap<String, List<String>> processLines(Scanner in)
+ 	{
+		Map<String, List<String>> iStore = new HashMap<String, List<String>>();
+		while(in.hasNextLine())
+		{
+			String[] words = in.nextLine().split("\\s");
+			LinkedList<String>  blacksmith = new LinkedList<String>();
+			LinkedList<String>  blob = new LinkedList<String>();
+			LinkedList<String>  grass = new LinkedList<String>();
+			LinkedList<String>  miner = new LinkedList<String>();
+			LinkedList<String>  obstacle = new LinkedList<String>();
+			LinkedList<String>  ore = new LinkedList<String>();
+			LinkedList<String>  quake = new LinkedList<String>();
+			LinkedList<String>  rocks = new LinkedList<String>();
+			LinkedList<String>  vein = new LinkedList<String>();
+			LinkedList<String>  background_default = new LinkedList<String>();
+
+			if(words[0] == "blacksmith")
+			{
+				blacksmith.add(words[1]);
+			}
+				if(words[0] == "blob")
+				{
+					blob.add(words[1]);
+				}
+				if(words[0] == "grass")
+				{
+					grass.add(words[1]);
+				}
+				if(words[0] == "miner")
+				{
+					miner.add(words[1]);
+				}
+				if(words[0] == "obstacle")
+				{
+					obstacle.add(words[1]);
+				}
+				if(words[0] == "ore")
+				{
+					ore.add(words[1]);
+				}
+				if(words[0] == "quake")
+				{
+					quake.add(words[1]);
+				}
+				if(words[0] == "rocks")
+				{
+					rocks.add(words[1]);
+				}
+				if(words[0] == "vein")
+				{
+					vein.add(words[1]);
+				}
+				if(words[0] == "background_default")
+				{
+					background_default.add(words[1]);
+				}
+			}
+		}
+		iStore.put("blacksmith", blacksmith);
+		iStore.put("blob", blob);
+		iStore.put("grass", grass);
+		iStore.put("miner", miner);
+		iStore.put("obstacle", obstacle);
+		iStore.put("ore", ore);
+      iStore.put("quake", quake);
+      iStore.put("rocks", rocks);
+      iStore.put("vein", vein);
+      iStore.put("background_default", background_default);
+      return iStore;
+ 	}
 
 
 	private static boolean verifyArgs(String[] args)
@@ -32,14 +124,7 @@ public class ImageStore
 
 	}
 
- 	private static void processLines(Scanner in)
- 	{
-		String name = "None";
-		while(in.hasNextLine())
-		{
-			String[] words = in.nextLine().split("\\s");
-		}
- 	}
+
 
 	public void processImageLine(List<String> images, String line)
 	{
@@ -76,4 +161,5 @@ public class ImageStore
 		}
 		return 0;
 	}
+	
 }
