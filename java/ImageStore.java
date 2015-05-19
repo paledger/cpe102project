592d5c04ce9334400b081dc794ddc78300314f1c
paledger <pledgerw@calpoly.edu>
2015-05-18 18:59:04 -0700

    started making ImageStore.java

--- java/ImageStore.java
+++ java/ImageStore.java
@@ -1,4 +1,10 @@
 public class ImageStore
 {
-	
+	private final static String DEFAULT_IMAGE_NAME = "background_default";
+	private final static int[] DEFAULT_IMAGE_COLOR = {128, 128, 128, 0};
+
+	public PImage createDefaultImage(int tile_width, int tile_height)
+	{
+		PImage surf = image()
+	}
 }
