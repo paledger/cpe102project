e7ced8d77a19da9b7a3bc5ae2891cb3f652828a0
paledger <pledgerw@calpoly.edu>
2015-05-19 19:30:05 -0700

    Revert "grrrrr"
    This reverts commit 7b932e37c5060ac91cf1cc12838d33fee7cf2867.

--- java/Entity.java
+++ java/Entity.java
@@ -45,7 +45,7 @@ public abstract class Entity extends PApplet
 	 }
 
 	 protected void scheduleEntity(WorldModel world, HashMap<String, LinkedList<PImage>> iStore)
-	 {}
+
 
 	 protected abstract String entityString()
 	 {};
